5e24f25fb3ac534323a3a69d4be63d123ae52786
SeYeol00 <dmot0126@gmail.com>
2023-08-28 13:47:14 +0900

    add: init

--- src/main/kotlin/com/review/moviesreviewservice/MoviesReviewServiceApplication.kt
+++ src/main/kotlin/com/review/moviesreviewservice/MoviesReviewServiceApplication.kt
@@ -7,5 +7,5 @@ import org.springframework.boot.runApplication
 class MoviesReviewServiceApplication
 
 fun main(args: Array<String>) {
-    runApplication<MoviesReviewServiceApplication>(*args)
+	runApplication<MoviesReviewServiceApplication>(*args)
 }
--- src/test/kotlin/com/review/moviesreviewservice/MoviesReviewServiceApplicationTests.kt
+++ src/test/kotlin/com/review/moviesreviewservice/MoviesReviewServiceApplicationTests.kt
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest
 @SpringBootTest
 class MoviesReviewServiceApplicationTests {
 
-    @Test
-    fun contextLoads() {
-    }
+	@Test
+	fun contextLoads() {
+	}
 
 }
