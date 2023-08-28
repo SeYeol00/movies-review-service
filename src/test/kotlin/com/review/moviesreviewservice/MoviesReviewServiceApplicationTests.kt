de33fa0d243d6623fd6075f8937a1ae5ddda3076
SeYeol00 <dmot0126@gmail.com>
2023-08-28 13:32:59 +0900

    repository init

---
+++ src/test/kotlin/com/review/moviesreviewservice/MoviesReviewServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.review.moviesreviewservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MoviesReviewServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
